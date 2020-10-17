38b51c4fe1cdf2bc9f40b92549ea6d2cddf3dbee
Michal Porag <miporagi@gmail.com>
2020-10-17 16:54:02 +0300

    password generator "wq
    </body>
    </body>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,84 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  password = "";
+  isLettersIncludes = false;
+  isNumbersIncludes = false;
+  isSymbolsIncludes = false;
+  passwordLength = 0;
+
+  onButtonClick() {
+    const numbers = "1234567890";
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const symbols = "!@#$%^&*()";
+    let validChars = "";
+
+    if (this.isLettersIncludes) {
+      validChars += letters;
+    }
+    if (this.isNumbersIncludes) {
+      validChars += numbers;
+    }
+    if (this.isSymbolsIncludes) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = "";
+    for (let i = 0; i < this.passwordLength; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
+
+  onCheckboxChange(dataToChagne) {
+    switch (dataToChagne) {
+      case "letters":
+        this.isLettersIncludes = !this.isLettersIncludes;
+        break;
+      case "numbers":
+        this.isNumbersIncludes = !this.isNumbersIncludes;
+        break;
+      case "symbols":
+        this.isSymbolsIncludes = !this.isSymbolsIncludes;
+        break;
+    
+      default:
+        console.error("Params not work");
+        break;
+    }
+  }
+
+  onChangeLength(e: string) {
+    const parsedValue = parseInt(e);
+    console.log("parsedValue: ", parsedValue);
+    
+    if (!isNaN(parsedValue)) {
+      this.passwordLength = parsedValue;
+    }
+    if (isNaN(parsedValue)) {
+      this.passwordLength = 0;
+    }
+  }
+
+  getPassword() {
+    return this.password;
+  }
+
+  getIsLettersIncludes() {
+    return this.isLettersIncludes;
+  }
+
+  getIsNumbersIncludes() {
+    return this.isNumbersIncludes;
+  }
+
+  getIsSymbolsIncludes() {
+    return this.isSymbolsIncludes;
+  }
+
+  getPasswordLength() {
+    return this.passwordLength;
+  }
 }
